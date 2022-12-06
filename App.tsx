427409793b40f276f434f878232e76216056a778
Matthes-Baer <Matthes.baer@yahoo.de>
2022-12-06 23:41:07 +0100

    feat: Added ScrollView and other stuff to try around with layout & design

--- App.tsx
+++ App.tsx
@@ -1,18 +1,48 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [text, setText] = useState<string>("Starting Text");
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: "row" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <TextInput style={styles.input} onChangeText={setText} value={text} />
-        <Text>{text} + 1</Text>
-        <Button title="This is a button" />
+        <Text style={{ flex: 1 }}>{text ? text : "placeholder..."}</Text>
+        <View style={{ flex: 1 }}>
+          <Button title="This is a button" />
+        </View>
       </View>
-      <Text style={styles.flexItem}>Test?</Text>
+      <View>
+        <ScrollView>
+          <Text style={styles.flexItem}>Test?</Text>
+          <Text style={styles.textStyle}>
+            There are UI-wise differences for android and iOS. Some elements
+            don't support a border-radius for both android and iOS, for example.
+            Therefore the View element should be used since this element
+            supports such styling behavior for both platforms.
+          </Text>
+          <Text style={styles.textStyle}>
+            In React Native child elements don't inherit the styling from its
+            parents. Setting the font-color for a parent element doesn't also
+            change the font-color for the respective child elements.
+          </Text>
+        </ScrollView>
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -26,6 +56,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "column",
+    marginTop: 100,
   },
   flexItem: {
     padding: 25,
@@ -35,5 +66,13 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    flex: 1,
+  },
+  textStyle: {
+    fontSize: 25,
+    padding: 25,
+    borderWidth: 2,
+    borderColor: "red",
+    margin: 10,
   },
 });
