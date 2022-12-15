8562bafdf32888c2ceebcca44393fdd289e4876d
Matthes-Baer <Matthes.baer@yahoo.de>
2022-12-15 16:53:56 +0100

    feat: Added useRoute TypeScript type

--- screens/SecondScreen.tsx
+++ screens/SecondScreen.tsx
@@ -5,18 +5,22 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 type Props = NativeStackScreenProps<StackParamList, "SecondScreen">;
 
 //! The useNavigation hook can be used when having to access the navigation method in a component that is not a screen component.
-//! The useRoute hook provides the same functions having it as a prop for the screen component but can be used for non screen components.
+//! The useRoute hook provides the same functions as having it as a prop for the screen component but can be used for non screen components. Helpful for receiving the route props, for example.
 //? Therefore in this case it's irrelevant since this is a screen component.
 import { useNavigation, useRoute } from "@react-navigation/native";
-import type { StackNavigationProp } from "@react-navigation/stack";
+import type { StackNavigationProp } from "@react-navigation/stack"; // for useNavigation hook
+import type { RouteProp } from "@react-navigation/native"; // for useRoute hook
 
 type SecondScreenNavigationProp = StackNavigationProp<
   StackParamList,
   "SecondScreen"
 >;
 
+type SecondScreenRouteProp = RouteProp<StackParamList, "SecondScreen">;
+
 export default function SecondScreen({ navigation, route }: Props) {
   const navigationHook = useNavigation<SecondScreenNavigationProp>();
+  const routeHook = useRoute<SecondScreenRouteProp>();
   return (
     <View>
       <Text>Second Screen Text</Text>
