f3a58c6de80f5edc6a601aa5b262565c088cf481
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-01 00:03:07 +0100

    feat: Added SQLite database logic for adding and fetching data

--- screens/nested-screens/SecondNested.tsx
+++ screens/nested-screens/SecondNested.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useState, useEffect } from "react";
 
 //? TypeScript stuff for React Navigation
 import type { NestedStackParamList } from "../../utils/ReactNavigationTypes";
@@ -8,11 +9,18 @@ type Props = NativeStackScreenProps<NestedStackParamList, "SecondNested">;
 //! The useNavigation hook can be used when having to access the navigation method in a component that is not a screen component.
 //! The useRoute hook provides the same functions as having it as a prop for the screen component but can be used for non screen components. Helpful for receiving the route props, for example.
 //? Therefore in this case it's irrelevant since this is a screen component.
-import { useNavigation, useRoute } from "@react-navigation/native";
+import {
+  useNavigation,
+  useRoute,
+  useIsFocused,
+} from "@react-navigation/native";
 import type { StackNavigationProp } from "@react-navigation/stack"; // for useNavigation hook
 import type { RouteProp } from "@react-navigation/native"; // for useRoute hook
 import ImagePicker from "../../components/ImagePicker";
 import LocationPicker from "../../components/LocationPicker";
+
+//? SQLite database functions
+import { init, insertData, fetchData } from "../../utils/database";
 
 type SecondNestedScreenNavigationProp = StackNavigationProp<
   NestedStackParamList,
@@ -25,12 +33,53 @@ export default function SecondScreen({ navigation, route }: Props) {
   //! Hooks are used to access the corresponding methods outside of screen components - in this case such hooks are therefore not necessary.
   const navigationHook = useNavigation<SecondNestedScreenNavigationProp>();
   const routeHook = useRoute<SecondScreenRouteProp>();
+  const isFocused = useIsFocused();
+
+  const [dbInitialized, setDbInitialized] = useState(false);
+  const [databaseData, setDatabaseData] = useState<any>();
+
+  useEffect(() => {
+    if (isFocused) {
+      init()
+        .then(() => setDbInitialized(true))
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
+
+  const fetchDatabaseData = async () => {
+    const result = await fetchData();
+    setDatabaseData(result);
+  };
 
   return (
     <View>
       <Text>Second Screen Text</Text>
       <ImagePicker />
       <LocationPicker />
+      <Button
+        title="insert data into databse with SQLite"
+        onPress={() =>
+          insertData({
+            title: "titleEntry",
+            description: "This is a description entry",
+          })
+        }
+      />
+      <Button title="fetch database Data" onPress={fetchDatabaseData} />
+      <View>
+        {databaseData &&
+          databaseData.map(
+            (e: { id: number; title: string; description: string }) => {
+              return (
+                <Text key={e.id}>
+                  {e.title}: {e.description}
+                </Text>
+              );
+            }
+          )}
+      </View>
     </View>
   );
 }
--- utils/database.ts
+++ utils/database.ts
@@ -4,23 +4,65 @@ import * as SQLite from "expo-sqlite";
 const database = SQLite.openDatabase("myDatabase.db");
 
 export const init = () => {
-  //   const promise = new Promise((resolve, reject) => {
-  //     database.transaction((tx) => {
-  //       tx.executeSql(
-  //         `CREATE TABLE IF NOT EXISTS myDatabase (
-  //                     id INTEGER PRIMARY KEY NOT NULL,
-  //                     title TEXT NOT NULL,
-  //                     description TEXT NOT NULL
-  //                 )`,
-  //         [],
-  //         () => {
-  //           resolve();
-  //         },
-  //         (_, error) => {
-  //           reject(error);
-  //         }
-  //       );
-  //     });
-  //   });
-  //   return promise;
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS myDatabase (
+                      id INTEGER PRIMARY KEY NOT NULL,
+                      title TEXT NOT NULL,
+                      description TEXT NOT NULL
+                  )`,
+        [],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+          return false;
+        }
+      );
+    });
+  });
+  return promise;
+};
+
+export const insertData = (data: { title: string; description: string }) => {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO myDatabase (title, description) VALUES (?, ?)`,
+        [data.title, data.description],
+        (_, result) => {
+          console.log(result);
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+          return false;
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `SELECT * FROM myDatabase`,
+        [],
+        (_, result) => {
+          resolve(result.rows._array);
+        },
+        (_, error) => {
+          reject(error);
+          return false;
+        }
+      );
+    });
+  });
+
+  return promise;
 };
